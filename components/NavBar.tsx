b2269e33b8c141e6739772bd88c27a2102648bd9
lazybump <104283344+lazybump@users.noreply.github.com>
2022-11-19 22:20:14 +0000

    build mobile and desktop navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav((prev) => !prev);
+  };
+
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex items-center justify-between w-full h-full px-2 2xl:px-16">
@@ -34,6 +43,83 @@ const NavBar = () => {
               </li>
             </Link>
           </ul>
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={
+          nav ? "md:hidden fixed top-0 left-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md: [45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex items-center justify-between">
+              <Image
+                src="/../public/assets/navLogo.png"
+                width="35"
+                height="5"
+                alt="/"
+              />
+              <div
+                onClick={handleNav}
+                className="p-3 rounded-full shadow-lg cursor-pointer shadow-gray-400"
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="my-4 border-b border-gray-300">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let's build something legendary together
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col py-4">
+            <ul className="uppercase">
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let's connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="p-3 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105">
+                  <FaLinkedinIn />
+                </div>
+                <div className="p-3 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105">
+                  <FaGithub />
+                </div>
+                <div className="p-3 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105">
+                  <AiOutlineMail />
+                </div>
+                <div className="p-3 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
