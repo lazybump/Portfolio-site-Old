dc99eca8445fb731035071c33b8be83d6ae8da68
lazybump <104283344+lazybump@users.noreply.github.com>
2022-11-20 16:22:10 +0000

    add skills section

---
+++ components/SkillCard.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+interface SkillCardProps {
+  name: string;
+  imgUrl: string;
+}
+
+const SkillCard = ({ name, imgUrl }: SkillCardProps) => {
+  return (
+    <div className="p-6 duration-300 ease-in shadow-xl rounded-xl hover:scale-110">
+      <div className="grid items-center justify-center grid-cols-2 gap-4">
+        <div className="m-auto">
+          <Image src={imgUrl} alt="/" width="64" height="64" />
+        </div>
+        <div className="text-center">
+          <h3 className="uppercase">{name}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkillCard;
---
+++ components/Skills.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import skills from "../public/data/skills.json";
+import SkillCard from "./SkillCard";
+
+const Skills: React.FC = () => {
+  return (
+    <div className="w-full p-2 lg:h-screen">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          {skills.map((skill) => (
+            <SkillCard key={skill.id} {...skill} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
