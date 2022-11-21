0d9ff88700055e007e82ad36a7ad24ecb1905f42
lazybump <104283344+lazybump@users.noreply.github.com>
2022-11-21 21:46:18 +0000

    add contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
+import Contact from "../components/Contact";
 import Main from "../components/Main";
 import NavBar from "../components/NavBar";
 import Projects from "../components/Projects";
@@ -19,6 +20,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
