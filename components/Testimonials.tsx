6d3df01460cfd4b45a0006852b330545bd635b65
lazybump <104283344+lazybump@users.noreply.github.com>
2022-12-09 07:20:21 +0000

    fix misplaced testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,12 +3,19 @@ import TestimonialItem from "./TestimonialItem";
 
 const Testimonials = () => {
   return (
-    <div id="testimonials" className="p-4 my-20">
-      <h2 className="py-4">Testimonials</h2>
-      <div className="flex flex-wrap justify-around">
-        {testimonials.map((testimonial) => (
-          <TestimonialItem key={testimonial.id} {...testimonial} />
-        ))}
+    // <div id="skills" className="w-full p-2 lg:h-screen">
+    // <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full"></div>
+    <div id="testimonials" className="w-full p-2 my-20">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-primary-color">
+          Testimonials
+        </p>
+        <h2 className="py-4 mb-10">Here what they have to say</h2>
+        <div className="flex flex-wrap justify-around">
+          {testimonials.map((testimonial) => (
+            <TestimonialItem key={testimonial.id} {...testimonial} />
+          ))}
+        </div>
       </div>
     </div>
   );
